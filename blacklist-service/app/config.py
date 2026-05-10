304e1336bdc29c7a58086f396d763ca1a60355d3
Jedom11 <j.dominguezp@uniandes.edu.co>
2026-05-10 18:14:55 -0500

    buildspec para CI/CD Fargate con Docker y ECR

--- blacklist-service/app/config.py
+++ blacklist-service/app/config.py
@@ -16,9 +16,10 @@ class Config:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD") or "")
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
+    DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME")
+    DB_SSLMODE = os.getenv("DB_SSLMODE", "require")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSLMODE}"
     )
